81b2c69601dba19ace3a3b39956868053c714e98
23sent <utkusagocak@gmail.com>
2023-04-06 18:46:18 +0300

    Updated Transform and Camera classes.

--- Lucent/src/core/camera.cpp
+++ Lucent/src/core/camera.cpp
@@ -1,9 +1,46 @@
 #include "camera.h"
 
-Camera::Camera(glm::vec3 postiton) : m_position{postiton}
+Camera::Camera(glm::vec3 position) : m_position{ position }
 {
 	m_projection = glm::mat4(1.0f);
 	m_view = glm::mat4(1.0f);
 
 	m_projection = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, 0.0f, 100.0f);
+
+	CalculateViewMatrix();
+}
+
+void Camera::CalculateViewMatrix() {
+	glm::mat4 transform =
+		glm::translate(glm::mat4(1.0f), m_position)	// Translation
+		* glm::toMat4(glm::quat(m_rotation));		// Rotation
+
+	m_view =  glm::inverse(transform);
+}
+
+glm::mat4 Camera::GetViewProjectionMatrix() {
+	return m_projection * m_view;
+}
+
+void Camera::SetPosition(glm::vec3 p)
+{
+	m_position = p;
+	CalculateViewMatrix();
+}
+
+void Camera::SetRotation(glm::vec3 r)
+{
+	m_rotation = r;
+	CalculateViewMatrix();
+}
+
+void Camera::Move(glm::vec3 p) {
+	glm::vec4 pos = glm::vec4(p, 1);
+	m_position = m_position + glm::vec3(glm::toMat4(glm::quat(m_rotation)) * pos);
+	CalculateViewMatrix();
+}
+
+void Camera::Rotate(glm::vec3 r) {
+	m_rotation += r;
+	CalculateViewMatrix();
 }
--- Lucent/src/core/transform.h
+++ Lucent/src/core/transform.h
@@ -2,15 +2,23 @@
 
 #include "component.h"
 #include "glm/glm.hpp"
+#include <glm/gtx/quaternion.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 class Transform : public Component
 {
 private:
-	glm::vec3 position;
-	glm::vec3 rotation;
-	glm::vec3 scale;
+	glm::vec3 position = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 rotation = { 0.0f, 0.0f, 0.0f };
+	glm::vec3 scale = { 1.0f, 1.0f, 1.0f };
 
 public:
 	Transform();
 	~Transform();
+
+	glm::mat4 GetTransform() {
+		return glm::translate(glm::mat4(1.0f), position)	// Translation
+			* glm::toMat4(glm::quat(rotation))				// Rotation
+			* glm::scale(glm::mat4(1.0f), scale);			// Scale
+	}
 };
