f7e86bc1a3905e5b44c74f12e3f3cb01c2403d0d
alperen ayyildiz <alperenayyildiz@outlook.com>
2023-04-15 17:21:50 +0300

    moved res folder into sandbox project

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -18,28 +18,28 @@ public:
 
 	void Update(float dt) override
 	{
-		if (Input::GetButton(GLFW_KEY_LEFT))
+		if (Input::GetButton(KeyCode::LeftArrow))
 		{
 			glm::vec3 p = transform->GetPosition();
 			glm::vec3 newPos = glm::vec3(p.x - (1.0f * speed * dt), p.y, p.z);
 			transform->SetPosition(newPos);
 		}
 
-		else if (Input::GetButton(GLFW_KEY_RIGHT))
+		else if (Input::GetButton(KeyCode::RightArrow))
 		{
 			glm::vec3 p = transform->GetPosition();
 			glm::vec3 newPos = glm::vec3(p.x + (1.0f * speed * dt), p.y, p.z);
 			transform->SetPosition(newPos);
 		}
 
-		if (Input::GetButton(GLFW_KEY_UP))
+		if (Input::GetButton(KeyCode::UpArrow))
 		{
 			glm::vec3 p = transform->GetPosition();
 			glm::vec3 newPos = glm::vec3(p.x, p.y + (1.0f * speed * dt), p.z);
 			transform->SetPosition(newPos);
 		}
 
-		else if (Input::GetButton(GLFW_KEY_DOWN))
+		else if (Input::GetButton(KeyCode::DownArrow))
 		{
 			glm::vec3 p = transform->GetPosition();
 			glm::vec3 newPos = glm::vec3(p.x, p.y - (1.0f * speed * dt), p.z);
@@ -50,8 +50,8 @@ public:
 
 class MyScene : public Scene {
 	void Start() override {
-		Resources::LoadTexture("../Lucent/res/container.jpg", false, "container");
-		Resources::LoadShader("../Lucent/res/shaders/sprite.vert", "../Lucent/res/shaders/sprite.frag", nullptr, "sprite");
+		Resources::LoadTexture("./res/container.jpg", false, "container");
+		Resources::LoadShader("./res/shaders/sprite.vert", "./res/shaders/sprite.frag", nullptr, "sprite");
 
 		for (int j = 0; j < 60; j++) {
 			for (int i = 0; i < 60; i++) {
