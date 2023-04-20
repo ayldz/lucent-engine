2ce02165cf58b9cc049abc0c6cee1a2502a72e6a
alperen ayyildiz <alperenayyildiz@outlook.com>
2023-04-20 19:17:42 +0300

    added physics world and components

---
+++ Lucent/src/physics/contact_listener.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <box2d/box2d.h>
+
+class LucentContactListener : public b2ContactListener 
+{
+
+};
--- Lucent/src/physics/physics2D.h
+++ Lucent/src/physics/physics2D.h
@@ -1,9 +1,21 @@
 #pragma once
 
+#include <box2d/b2_body.h>
 #include <box2d/b2_world.h>
 
+#include "contact_listener.h"
 
-class Physics2D 
+class PhysicsWorld2D
 {
+private:
+	b2Vec2 m_gravity = b2Vec2(0.0f, -9.8f);
+	b2World *m_world = new b2World(m_gravity);
 
+	float m_physicsTime;
+	float physicsTimeStep;
+
+	PhysicsWorld2D()
+	{
+		m_world->SetContactListener(new LucentContactListener());
+	}
 };
